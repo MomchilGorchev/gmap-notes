cb38f0cd7f151819abc3b0f2eed7b96ae0af29a4
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-05-08 14:35:28 +0100

    Adding map markers

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,35 @@
 function initialize() {
+    var myLatlng = new google.maps.LatLng(51.5072, 0.1275);
     var mapOptions = {
-        center: { lat: 31.791702, lng: -7.092620},
+        center: myLatlng,
         zoom: 3,
         styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}]    };
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
+
+    var contentString = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">Home</h1>'+
+        '<div id="bodyContent">'+
+        '<p><b>London, UK</b><br />'+
+        'God save the queen!'+
+        '</p>'+
+        '</div>'+
+        '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    var marker_london = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Where I live now'
+    });
+    google.maps.event.addListener(marker_london, 'click', function() {
+        infowindow.open(map,marker_london);
+    });
+
 }
 google.maps.event.addDomListener(window, 'load', initialize);
